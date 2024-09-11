eea3b9f29c5f3237a5651dd9188a6a9b9e10028e
Andrey Maximov <hdsh138720@gmail.com>
2024-09-11 18:17:19 +0000

    add jll sectio + footer

--- README.md
+++ README.md
@@ -13,10 +13,20 @@ git push origin main
 
 Обновите GitHub Pages:
 npm run deploy
-
+_____________________________________
 
 git add .
 git commit -m "Описание"
 git push origin main
 npm run build
 npm run deploy
+
+
+ДЛЯ АНИМИРОВАНИЯ СТРАНИЦІ 
+
+transition: color 0.3s ease; 
+не на ховер 
+transition: transform 0.3s ease, color 0.3s ease; 
+
+transform: scale(1.15);
+transition: color 0.3s ease; 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello, Parcel!");
+document.addEventListener("DOMContentLoaded", () => {
+  const element = document.querySelector(".cloud_games-player");
+
+  function startRotation() {
+    element.classList.add("rotate");
+  }
+
+  function stopRotation() {
+    element.classList.remove("rotate");
+
+    void element.offsetWidth;
+    element.classList.add("rotate");
+  }
+
+  element.addEventListener("mouseenter", startRotation);
+
+  element.addEventListener("focus", startRotation);
+
+  element.addEventListener("mouseleave", () => {});
+
+  element.addEventListener("blur", () => {});
+
+  element.addEventListener("animationend", () => {
+    element.classList.remove("rotate");
+  });
+});
